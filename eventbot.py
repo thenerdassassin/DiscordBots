6b22232b02c504167375caa561df3400c85e0155
Joshua Schmidt <thenerdassassin@pm.me>
2022-05-13 10:07:46 -0500

    Add buyer and seller information to the Embed (#6)
    * Updated the format of the Embed
    * Fixed a bug for bundles by skipping them
    * Added link to buyer's OpenSea profile
    * Added linke to seller's OpenSea profile

--- eventbot.py
+++ eventbot.py
@@ -22,29 +22,56 @@ def getEvents(startTime, endTime, contractAddress, eventType):
     return requests.request("GET", url, headers=headers)
 
 
-# Convert OpenSea price to ETH in String format, i.e. 3.2 Ξ
+# Convert OpenSea price to ETH in String format, i.e. Ξ3.2 
 def convertPriceToETH(price):
-    return f'{float(float(price)/1000000000000000000.0):g} Ξ'
+    return f'Ξ{float(float(price)/1000000000000000000.0):g}'
 
 # Parse OpenSea Event for name of the asset
 # See: https://docs.opensea.io/reference/asset-object
 def getTitleFromEvent(event):
     return event.get("asset").get("name")
 
+def getUsernameFromEvent(event, userKey):
+    username = event.get(userKey).get("user").get("username")
+    userProfileLink = f'https://opensea.io/{event.get(userKey).get("address")}'
+    if (username):
+        return f'[{username}]({userProfileLink})'
+    
+
+    return f'[Unnamed]({userProfileLink})'
+
+# Parse OpenSea Event for the link to the asset
+# See: https://docs.opensea.io/reference/asset-object
+def getLinkToOpenSea(event):
+    return event.get("asset").get("permalink")
+
+# Parse OpenSea Event to create an Embed with title, description,
+# and url. See: https://discordpy.readthedocs.io/en/stable/api.html#embed
+def createEmbed(event, eventType):
+    if (eventType == 'successful'):
+        return DiscordEmbed(
+            title=f'{getTitleFromEvent(event)} was purchased!', 
+            description=f'Amount {convertPriceToETH(event.get("total_price"))}',
+            url=getLinkToOpenSea(event)
+        )
+    elif (eventType == 'created'):
+        return DiscordEmbed(
+            title=f'{getTitleFromEvent(event)} was listed!', 
+            description=f'Amount {convertPriceToETH(event.get("starting_price"))}',
+            url=getLinkToOpenSea(event)
+        )
+
 # Parse OpenSea Event to create a discord Embed
 # See: https://discordpy.readthedocs.io/en/stable/api.html#embed
 def convertEventToEmbed(event, eventType):
-    if (eventType == 'successful'):
-        embedTitle = f'{getTitleFromEvent(event)} was purchased!'
-    elif (eventType == 'created'):
-        embedTitle = f'{getTitleFromEvent(event)} was listed!'
-    embed = DiscordEmbed(title=embedTitle, url=event.get("asset").get("permalink"))
+    embed = createEmbed(event, eventType)
     embed.set_image(url=event.get("asset").get("image_original_url"))
 
     if (eventType == 'successful'):
-        embed.add_embed_field(name="Sale Price", value=convertPriceToETH(event.get("total_price")), inline=False)
-    elif (eventType == 'created'):
-        embed.add_embed_field(name="List Price", value=convertPriceToETH(event.get("ending_price")), inline=False)
+        embed.add_embed_field(name='Buyer', value=f'{getUsernameFromEvent(event, "winner_account")}', inline=True)
+
+    embed.add_embed_field(name='Seller', value=f'{getUsernameFromEvent(event, "seller")}', inline=True)
+    embed.add_embed_field(name='Link', value=f'[OpenSea]({getLinkToOpenSea(event)})', inline=True)
 
     return embed
         
@@ -56,7 +83,7 @@ def run_bot_sync(eventType):
     webhook = DiscordWebhook(url=config.webhook_url.get(eventType))
     
     for collection in config.collection_addresses:
-        #Get Listings
+        # Get Listings
         print(f'Getting {eventType} events for {collection}')
         currentEvents = getEvents(start_time, end_time, collection, eventType)
         if (currentEvents.status_code != 200):
@@ -66,8 +93,11 @@ def run_bot_sync(eventType):
             return
         currentEvents = currentEvents.json()
 
-        #Send via Webhook        
+        # Send via Webhook        
         for event in currentEvents['asset_events']:
+            # Skipping Bundles
+            if (not event.get("asset")):
+                continue
             print(f'Found {eventType} event for {getTitleFromEvent(event)}')
             embed = convertEventToEmbed(event, eventType)
             webhook.add_embed(embed)
